a40f3de64bb69841480f5596aa41cab667be1874
Alex Lam <st19004@ormiston.school.nz>
2021-07-27 15:11:05 +1200

    Update match_page.py

--- src/match_page.py
+++ src/match_page.py
@@ -12,7 +12,7 @@ class Page(BasePage):
 		title_section = Frame(self, bg=self.COLOURSCHEME[3])
 		title_section.grid(column=0, row=0, sticky=NSEW)
 
-		title = Label(title_section, text="Level {level} {difficulty}", font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
+		title = Label(title_section, text="XXX+XXX=", font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
 		title.grid(column=0, row=0)
 
 
@@ -25,7 +25,7 @@ class Page(BasePage):
 		user_header = Frame(header_section, bg=self.COLOURSCHEME[2])
 		user_header.grid(column=0, row=0, sticky=NSEW)
 
-		user_header_label = Label(user_header, text="User", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		user_header_label = Label(user_header, text="Time left", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
 		user_header_label.pack(side=LEFT)
 
 		score_header = Frame(header_section, bg=self.COLOURSCHEME[2])
@@ -44,15 +44,20 @@ class Page(BasePage):
 		content_section.rowconfigure(0, weight=1)
 
 		calculator_frame = Frame(content_section, bg=self.COLOURSCHEME[1])
-		calculator_frame.grid(column=1, row=0, sticky=NSEW)
+		calculator_frame.grid(column=1, row=0, sticky=NSEW, pady=30)
 		padding_frame_left = Frame(content_section, bg=self.COLOURSCHEME[1])
 		padding_frame_left.grid(column=0, row=0, sticky=NSEW)
 		padding_frame_right = Frame(content_section, bg=self.COLOURSCHEME[1])
 		padding_frame_right.grid(column=2, row=0, sticky=NSEW)
 
-		for col in range(3):
-			for row in range(3):
+		for row in range(3):
+			for col in range(3):
 				calculator_frame.columnconfigure(col, weight=1)
 				calculator_frame.rowconfigure(row, weight=1)
-				label = Label(calculator_frame, text=str(col*3+row+1), bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT)
-				label.grid(column=col, row=row, sticky=NSEW)
+				label = Label(calculator_frame, text=str(row*3+col+1), bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+				label.grid(column=col, row=row, sticky=NSEW, padx=10, pady=10)
+		calculator_frame.rowconfigure(3, weight=1)
+		label = Label(calculator_frame, text="0", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+		label.grid(column=0, row=3, sticky=NSEW, padx=10, pady=10)
+		label = Label(calculator_frame, text="Enter", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+		label.grid(column=1, row=3, sticky=NSEW, columnspan=2, padx=10, pady=10)
