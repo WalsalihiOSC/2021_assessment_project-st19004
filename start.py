6d10f90a7c051c41a8dcca602db6eded452a49dc
Alex Lam <st19004@ormiston.school.nz>
2021-09-22 18:23:01 +1200

    Better code for start.py

--- start.py
+++ start.py
@@ -3,22 +3,22 @@ from src import start_page, match_page, leaderboard_page
 from src.base_page import BasePage
 
 class Program(Tk):
-	def __init__(self, initial_page: BasePage) -> None:
-		super().__init__()
-
+	def start(self, initial_page: BasePage) -> None:
+		"""Starting of the program that accepts an inital page"""
 		self.pages: list[BasePage] = []
 
 		if initial_page is not None:
 			self.append_page(start_page)
 		
 		self.geometry("1200x700")
+		self.mainloop()
 	
 	def append_page(self, page: BasePage, *args, **kwargs) -> None:
 		"""Places page on top"""
 		# An empty list is implicitly false
 		if self.pages:
 			self.pages[-1].pack_forget()
-
+		
 		page = page.Page(master=self, *args, **kwargs)
 		page.pack(fill=BOTH, expand=True)
 		self.pages.append(page)
@@ -31,7 +31,7 @@ class Program(Tk):
 			self.pages[-1].pack(fill=BOTH, expand=True)
 		return page
 	
-	def show_page(self, wanted_page: str, *args, **kwargs):
+	def show_page(self, wanted_page: str, *args, **kwargs) -> None:
 		"""
 		Shows a specific defined page:
 			start
@@ -51,5 +51,8 @@ class Program(Tk):
 		
 		self.append_page(page, *args, **kwargs)
 
-root = Program(start_page)
-root.mainloop()
+def main() -> None:
+	root = Program().start(start_page)
+
+if __name__ == "__main__":
+	main()
