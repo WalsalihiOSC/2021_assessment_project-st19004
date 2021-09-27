526369659e236d8b259432a9597059178e0873e9
Alex Lam <st19004@ormiston.school.nz>
2021-09-27 17:14:16 +1300

    Less verbose naming for leaderboard_page.py and match_page.py

--- src/leaderboard_page.py
+++ src/leaderboard_page.py
@@ -8,41 +8,41 @@ class Page(BasePage):
 		self.columnconfigure(0, weight=1) # Ensure the frame size stretches to fit horizontally
 
 		# Title section
-		title_section = Frame(self, bg=self.COLOURSCHEME[3])
-		title_section.grid(column=0, row=0, sticky=NSEW)
+		title = Frame(self, bg=self.COLOURSCHEME[3])
+		title.grid(column=0, row=0, sticky=NSEW)
 
-		title = Label(title_section, text="Level {level} {difficulty}", font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
-		title.grid(column=0, row=0)
+		title.label = Label(title, text="Level {level} {difficulty}", font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
+		title.label.grid(column=0, row=0)
 
-		back_button = BackButton(title_section, command=self.page_back, bg=self.COLOURSCHEME[3], relief="flat")
+		back_button = BackButton(title, command=self.page_back, bg=self.COLOURSCHEME[3], relief="flat")
 		back_button.place(relx=1, rely=0, x=-10, y=10, anchor="ne")
 
 		# Header section
-		header_section = Frame(self, bg=self.COLOURSCHEME[2])
-		header_section.grid(column=0, row=1, sticky=NSEW)
-		header_section.columnconfigure(0, weight=4)
-		header_section.columnconfigure(1, weight=1)
+		header = Frame(self, bg=self.COLOURSCHEME[2])
+		header.grid(column=0, row=1, sticky=NSEW)
+		header.columnconfigure(0, weight=4)
+		header.columnconfigure(1, weight=1)
 
-		user_header = Frame(header_section, bg=self.COLOURSCHEME[2])
-		user_header.grid(column=0, row=0, sticky=NSEW)
+		header.user = Frame(header, bg=self.COLOURSCHEME[2])
+		header.user.grid(column=0, row=0, sticky=NSEW)
 
-		user_header_label = Label(user_header, text="User", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
-		user_header_label.pack(side=LEFT)
+		header.user.label = Label(header.user, text="User", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		header.user.label.pack(side=LEFT)
 
-		score_header = Frame(header_section, bg=self.COLOURSCHEME[2])
-		score_header.grid(column=1, row=0, sticky=NSEW)
+		header.score = Frame(header, bg=self.COLOURSCHEME[2])
+		header.score.grid(column=1, row=0, sticky=NSEW)
 
-		score_header_label = Label(score_header, text="Score", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
-		score_header_label.pack(side=LEFT)
+		header.score.label = Label(header.score, text="Score", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		header.score.label.pack(side=LEFT)
 
 		# Content section
-		content_section = Frame(self, bg=self.COLOURSCHEME[1])
-		content_section.grid(column=0, row=2, sticky=NSEW)
-		content_section.columnconfigure(0, weight=4)
-		content_section.columnconfigure(1, weight=1)
+		content = Frame(self, bg=self.COLOURSCHEME[1])
+		content.grid(column=0, row=2, sticky=NSEW)
+		content.columnconfigure(0, weight=4)
+		content.columnconfigure(1, weight=1)
 
-		user_content = Frame(content_section, bg=self.COLOURSCHEME[1])
-		user_content.grid(column=0, row=0, sticky=NSEW)
+		content.user = Frame(content, bg=self.COLOURSCHEME[1])
+		content.user.grid(column=0, row=0, sticky=NSEW)
 
-		score_content = Frame(content_section, bg=self.COLOURSCHEME[1])
-		score_content.grid(column=1, row=0, sticky=NSEW)
+		content.score = Frame(content, bg=self.COLOURSCHEME[1])
+		content.score.grid(column=1, row=0, sticky=NSEW)
--- src/match_page.py
+++ src/match_page.py
@@ -25,78 +25,78 @@ class Page(BasePage):
 		self.init_answer()
 	
 	def init_title_section(self) -> None:
-		self.title_section = Frame(self, bg=self.COLOURSCHEME[3])
-		self.title_section.grid(column=0, row=0, sticky=NSEW)
+		self.title = Frame(self, bg=self.COLOURSCHEME[3])
+		self.title.grid(column=0, row=0, sticky=NSEW)
 
-		self.title = Label(self.title_section, font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
-		self.title.grid(column=0, row=0)
+		self.title.label = Label(self.title, font=self.TITLE_FONT, bg=self.COLOURSCHEME[3], fg=self.COLOURSCHEME[1])
+		self.title.label.grid(column=0, row=0)
 
-		back_button = BackButton(self.title_section, command=self.page_back, bg=self.COLOURSCHEME[3], relief="flat")
+		back_button = BackButton(self.title, command=self.page_back, bg=self.COLOURSCHEME[3], relief="flat")
 		back_button.place(relx=1, rely=0, x=-10, y=10, anchor="ne")
 	
 	def init_header_section(self) -> None:
-		self.header_section = Frame(self, bg=self.COLOURSCHEME[2])
-		self.header_section.grid(column=0, row=1, sticky=NSEW)
-		self.header_section.columnconfigure(0, weight=4)
-		self.header_section.columnconfigure(1, weight=1)
+		self.header = Frame(self, bg=self.COLOURSCHEME[2])
+		self.header.grid(column=0, row=1, sticky=NSEW)
+		self.header.columnconfigure(0, weight=4)
+		self.header.columnconfigure(1, weight=1)
 
-		user_header = Frame(self.header_section, bg=self.COLOURSCHEME[2])
-		user_header.grid(column=0, row=0, sticky=NSEW)
+		self.header.user = Frame(self.header, bg=self.COLOURSCHEME[2])
+		self.header.user.grid(column=0, row=0, sticky=NSEW)
 
-		user_header_label = Label(user_header, text="Time left", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
-		user_header_label.pack(side=LEFT)
+		self.header.user.label = Label(self.header.user, text="Time left", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		self.header.user.label.pack(side=LEFT)
 
-		score_header = Frame(self.header_section, bg=self.COLOURSCHEME[2])
-		score_header.grid(column=1, row=0, sticky=NSEW)
-		score_header.columnconfigure(0, weight=1)
-		score_header.columnconfigure(1, weight=1)
+		self.header.score = Frame(self.header, bg=self.COLOURSCHEME[2])
+		self.header.score.grid(column=1, row=0, sticky=NSEW)
+		self.header.score.columnconfigure(0, weight=1)
+		self.header.score.columnconfigure(1, weight=1)
 
-		score_header_label = Label(score_header, text="Score", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
-		score_header_label.grid(column=0, row=0, sticky=NSEW)
-		score_header_value = Label(score_header, text="0", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
-		score_header_value.grid(column=1, row=0, sticky=NSEW)
+		self.header.score.label = Label(self.header.score, text="Score", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		self.header.score.label.grid(column=0, row=0, sticky=NSEW)
+		self.header.score.value = Label(self.header.score, text="0", font=self.HEADER_FONT, bg=self.COLOURSCHEME[2], fg=self.COLOURSCHEME[1])
+		self.header.score.value.grid(column=1, row=0, sticky=NSEW)
 		# _a, _b, _c variable are irrelevant to updating the score header
-		self.score.trace_add("write", lambda _a, _b, _c: score_header_value.config(text=self.score.get()))
+		self.score.trace_add("write", lambda _a, _b, _c: self.header.score.value.config(text=self.score.get()))
 
 	def init_content_section(self) -> None:
-		self.content_section = Frame(self, bg=self.COLOURSCHEME[1])
-		self.content_section.grid(column=0, row=2, sticky=NSEW)
-		self.content_section.columnconfigure(0, weight=1)
-		self.content_section.columnconfigure(1, weight=1)
-		self.content_section.columnconfigure(2, weight=1)
-		self.content_section.rowconfigure(0, weight=1)
+		self.content = Frame(self, bg=self.COLOURSCHEME[1])
+		self.content.grid(column=0, row=2, sticky=NSEW)
+		self.content.columnconfigure(0, weight=1)
+		self.content.columnconfigure(1, weight=1)
+		self.content.columnconfigure(2, weight=1)
+		self.content.rowconfigure(0, weight=1)
 
 		self.init_calculator()
 
 	def init_calculator(self) -> None:
 		# Calculator
-		calculator_frame = Frame(self.content_section, bg=self.COLOURSCHEME[1])
-		calculator_frame.grid(column=1, row=0, sticky=NSEW, pady=30)
+		calculator = Frame(self.content, bg=self.COLOURSCHEME[1])
+		calculator.grid(column=1, row=0, sticky=NSEW, pady=30)
 
 		# Padding to keep calculator centred
-		padding_frame_left = Frame(self.content_section, bg=self.COLOURSCHEME[1])
+		padding_frame_left = Frame(self.content, bg=self.COLOURSCHEME[1])
 		padding_frame_left.grid(column=0, row=0, sticky=NSEW)
-		padding_frame_right = Frame(self.content_section, bg=self.COLOURSCHEME[1])
+		padding_frame_right = Frame(self.content, bg=self.COLOURSCHEME[1])
 		padding_frame_right.grid(column=2, row=0, sticky=NSEW)
 
 		# Initialisation for the calculator's buttons
 		for row in range(3):
 			for col in range(3):
-				calculator_frame.columnconfigure(col, weight=1)
-				calculator_frame.rowconfigure(row, weight=1)
-				label = Label(calculator_frame, text=str(row*3+col+1), bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+				calculator.columnconfigure(col, weight=1)
+				calculator.rowconfigure(row, weight=1)
+				label = Label(calculator, text=str(row*3+col+1), bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
 				label.grid(column=col, row=row, sticky=NSEW, padx=10, pady=10)
 		# Initialisation for the last 2 buttons
 		# Should be changed to reduce duplication
-		calculator_frame.rowconfigure(3, weight=1)
-		label = Label(calculator_frame, text="0", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+		calculator.rowconfigure(3, weight=1)
+		label = Label(calculator, text="0", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
 		label.grid(column=0, row=3, sticky=NSEW, padx=10, pady=10)
-		label = Label(calculator_frame, text="Enter", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
+		label = Label(calculator, text="Enter", bg=self.COLOURSCHEME[1], font=self.CONTENT_FONT, borderwidth=1, relief="solid")
 		label.grid(column=1, row=3, sticky=NSEW, columnspan=2, padx=10, pady=10)
 
 	def init_answer(self) -> None:
 		self.answer_var = answer_var = StringVar()
-		answer_box = Entry(self.title_section, width=6, font=self.TITLE_FONT, bg=self.COLOURSCHEME[1], fg=self.COLOURSCHEME[0], justify=CENTER, textvariable=answer_var, validate="key", validatecommand=(self.register(self.validate_input), "%P"))
+		answer_box = Entry(self.title, width=6, font=self.TITLE_FONT, bg=self.COLOURSCHEME[1], fg=self.COLOURSCHEME[0], justify=CENTER, textvariable=answer_var, validate="key", validatecommand=(self.register(self.validate_input), "%P"))
 		answer_box.grid(column=1, row=0)
 		answer_box.bind("<Return>", self.answer)
 		self.generate_question()
@@ -123,7 +123,7 @@ class Page(BasePage):
 		num1 = randint(1*10**(self.level-1), 1*10**self.level)
 		num2 = randint(1*10**(self.level-1), 1*10**self.level)
 		equation = f"{num1}{self.operator}{num2}"
-		self.title.config(text=f"{equation}=")
+		self.title.label.config(text=f"{equation}=")
 		self.answer_var.set("")
 		# Evaluate equation and turn the number into a string
 		self.correct_answer = correct_answer = str(eval(equation))
